12c22ce57067d276214a7ccfedb3b9ed6e638c4a
edenfwu <zzxjl1@hotmail.com>
2026-02-19 19:10:09 +0800

    修改allowedHosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     port: 3100,
     host: '0.0.0.0',
+    allowedHosts: ['video-voice-translator.idealbroker.cn'],
   },
   plugins: [react()],
   resolve: {
